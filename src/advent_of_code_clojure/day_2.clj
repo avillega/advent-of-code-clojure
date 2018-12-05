e7886f044909be46dfe9679ba656e928c448de88
AVillegasP <villegas.pelaez.andres@gmail.com>
2018-12-05 18:50:17 -0500

    Start working on day 2

---
+++ src/advent_of_code_clojure/day_2.clj
@@ -0,0 +1,10 @@
+(ns advent-of-code-clojure.day-2
+  (:require [clojure.string :as str]))
+
+(def ids (->> (slurp "resources/input-day-2.txt")
+              (str/split-lines)))
+
+(def solve []
+  )
+
+              
